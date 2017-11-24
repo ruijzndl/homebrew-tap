d5731992bab6d54e6d056bb85c1bc45df17dfeda
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2017-11-24 17:40:58 +0100

    master: updated version to 0.5.5 and sha256

--- Formula/tacs.rb
+++ Formula/tacs.rb
@@ -1,8 +1,9 @@
 class Notes < Formula
   desc "..."
   homepage "https://github.com/ruijzndl/tacs"
-  url "https://github.com/ruijzndl/tacs/archive/0.5.4.tarball.gz"
-  version "0.5.4"
+  url "https://github.com/ruijzndl/tacs/archive/0.5.5.tarball.gz"
+  version "0.5.5"
+  sha256 6d72c30778a5882bbb664763cbc79dcac0e5f36766a2db3d57c80e5e1b0b7756
 
   def install
     bin.install "bin/tacs"
