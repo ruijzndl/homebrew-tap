8ca75622f16821b92a8f28f1c40fce1a13461c43
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2017-11-24 17:47:21 +0100

    master: added surrounding quotes to sha256 hash

--- Formula/tacs.rb
+++ Formula/tacs.rb
@@ -3,7 +3,7 @@ class Notes < Formula
   homepage "https://github.com/ruijzndl/tacs"
   url "https://github.com/ruijzndl/tacs/archive/0.5.5.tarball.gz"
   version "0.5.5"
-  sha256 6d72c30778a5882bbb664763cbc79dcac0e5f36766a2db3d57c80e5e1b0b7756
+  sha256 "6d72c30778a5882bbb664763cbc79dcac0e5f36766a2db3d57c80e5e1b0b7756"
 
   def install
     bin.install "bin/tacs"
