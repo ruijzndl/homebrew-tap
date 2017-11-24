65949924f1cd09b02fef7ae8a3c45cdc47ba774f
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2017-11-24 17:51:27 +0100

    master: updated reference to tarball

--- Formula/tacs.rb
+++ Formula/tacs.rb
@@ -1,7 +1,7 @@
 class Tacs < Formula
   desc "Todo"
   homepage "https://github.com/ruijzndl/tacs"
-  url "https://github.com/ruijzndl/tacs/archive/0.5.5.tarball.gz"
+  url "https://github.com/ruijzndl/tacs/archive/0.5.5.tar.gz"
   version "0.5.5"
   sha256 "6d72c30778a5882bbb664763cbc79dcac0e5f36766a2db3d57c80e5e1b0b7756"
 
