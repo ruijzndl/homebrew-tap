9a723f8f4d0f8d049fabb23da6f0e4104458e738
Danny Ruijzendaal <danny.ruijzendaal@philips.com>
2017-11-24 17:26:40 +0100

    master: initial commit containing README and formula

---
+++ Formula/tacs.rb
@@ -0,0 +1,11 @@
+class Notes < Formula
+  desc "..."
+  homepage "https://github.com/ruijzndl/tacs"
+  url "https://github.com/ruijzndl/tacs/archive/0.5.4.tarball.gz"
+  version "0.5.4"
+
+  def install
+    bin.install "bin/tacs"
+  end
+end
+
---
+++ README.md
@@ -0,0 +1,12 @@
+## homebrew-tap
+Homebrew tap with formula for installing the following:
+
+- [tacs](https://github.com/ruijzndl/tacs)
+
+
+#### Add this tap
+`brew tap ruijzndl/tap`
+
+
+#### Install
+- tacs: `brew install tacs`
